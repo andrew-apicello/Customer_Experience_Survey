6bd3b38dbae51eae9fe959194add7af6eefe15ef
andrew.apicello@gmail.com <andrew.apicello@gmail.com>
2017-10-05 22:09:21 -0400

    Update Server Post Environment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
